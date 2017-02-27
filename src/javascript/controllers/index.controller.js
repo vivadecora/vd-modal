a8b07e1c3fb426c72d814441b347c02343264757
Jota Teles <josetelesmaciel@gmail.com>
2017-02-27 18:27:52 -0300

    [#6] adiciona modal de sidebar

--- src/javascript/controllers/index.controller.js
+++ src/javascript/controllers/index.controller.js
@@ -6,13 +6,24 @@
 	function indexController(ModalModel){
 		var vc = this;
 		vc.simpleModal = new ModalModel.getConstructor("simple-modal");
+		vc.helloWorlModal = new ModalModel.getConstructor("hello-world");
+		vc.sidebarModal = new ModalModel.getConstructor("sidebar-modal");
+		vc.flexboxModal = new ModalModel.getConstructor("flexbox-modal");
+		vc.sidebarModal.isFullscreen = true;
+		
 		vc.openSimpleModal = function(){
 			vc.simpleModal.isOpen = true;
 		}
 
-		vc.helloWorlModal = new ModalModel.getConstructor("hello-world");
+		vc.openFlexboxModal = function(){
+			vc.flexboxModal.isOpen = true;
+		}
+
 		vc.openHelloWorlModal = function(){
 			vc.helloWorlModal.isOpen = true;
+		}
+		vc.openSidebarModal = function(){
+			vc.sidebarModal.isOpen = true;
 		}
 
 	}
